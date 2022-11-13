10be7341375fec3167067e3e6efa6b79b857a908
phaneendra <phaneendra.kaddi@gmail.com>
2022-11-13 13:39:33 +0530

    ping response

--- app/server.go
+++ app/server.go
@@ -18,9 +18,30 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Failed to read from connection", err.Error())
+			return
+		}
+		fmt.Println("Received", string(buf[:n]))
+		if string(buf[:n]) == "PING" {
+			conn.Write([]byte("PONG"))
+		}
 	}
 }
